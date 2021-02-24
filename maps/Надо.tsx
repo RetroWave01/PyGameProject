bfcd6f4f82c1c5e34c6fb4a6e64d694aefc9a5b4
RetroWave01 <74599863+RetroWave01@users.noreply.github.com>
2021-02-24 20:12:20 +0500

    Add files via upload

---
+++ maps/Надо.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Надо" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="54" columns="9">
+ <image source="../../Надо.png" width="298" height="199"/>
+</tileset>
